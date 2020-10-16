0b2843bc4241ae4510cd26ab2edd17c182793c94
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-10-16 15:33:28 -0500

    Updates

---
+++ precache-manifest.8a7fac362b31ee1764e9f3af78dda213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b8276b22059723d7010483b3658292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e86298470c3ca7420eeb",
+    "url": "/static/css/main.86a57a57.chunk.css"
+  },
+  {
+    "revision": "fc8b9fae94d00758ab4a",
+    "url": "/static/js/2.21cb6fbe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.21cb6fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86298470c3ca7420eeb",
+    "url": "/static/js/main.0fa8b5fd.chunk.js"
+  },
+  {
+    "revision": "8c7883f0cd3522644025",
+    "url": "/static/js/runtime-main.5464b3d5.js"
+  }
+]);
